56ac1262df6495aad2d9edb11ccbeb896ae5b744
Saurav Pant <pantsaurav005@gmail.com>
2023-07-25 20:12:33 +0530

    Done with home page

--- src/app/Home/Page.tsx
+++ src/app/Home/Page.tsx
@@ -7,7 +7,6 @@ const Home = (props: Props) => {
     <div className="flex flex-col items-center lg:flex-row lg:justify-between lg:h-[80vh] lg:mt-16 px-4  md:px-16 lg:px-24 mt-10">
       {/* Left Side */}
       <div className="lg:w-1/2 lg:flex lg:flex-col lg:items-start lg:justify-center lg:pr-8">
-        {/* Move the heading down in small devices */}
         <h1 className="text-4xl font-bold mb-4 sm:mb-6">Welcome to CollegeReviewHub</h1>
         <p className="text-lg mb-10">
           Share and read honest reviews about colleges from students like you.
@@ -18,7 +17,6 @@ const Home = (props: Props) => {
         </button>
       </div>
 
-      {/* Right Side */}
       <div className="lg:w-1/2">
         <img
           src="https://images.unsplash.com/photo-1535982330050-f1c2fb79ff78?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y29sbGVnZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=1400&q=60"

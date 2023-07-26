4c460837bc0c10ffe6919f618fbb5c0703a4f80b
Saurav Pant <pantsaurav005@gmail.com>
2023-07-26 08:24:13 +0530

    Footer fixed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="flex justify-center items-center h-16 font-bold mt-10">
+    <div className="flex justify-center items-center h-16 font-bold mt-10 fixed bottom-0 left-0 right-0 text-center">
       <p>
         Made With 💗 by
         <span className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text text-transparent pl-2">

87dcc78031ba86c29b76452a5286c07244a8c882
Saurav Pant <pantsaurav005@gmail.com>
2023-07-27 11:51:03 +0530

    Login Image Added

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -1,52 +1,63 @@
-import BackButton from "@/components/BackButton";
 import React from "react";
+import BackButton from "@/components/BackButton";
 
 const Login = () => {
   return (
-    <div className="flex justify-center items-center h-[90vh] relative">
-      <BackButton link="/"/>
-      <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <h1 className="text-4xl font-bold mb-4 sm:mb-6
-        bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text text-transparent
-        ">Login</h1>
-        <form>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="username"
-            >
-              Username
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="username"
-              type="text"
-              placeholder="Username"
-            />
-          </div>
-          <div className="mb-6">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="password"
-              type="password"
-              placeholder="Password"
-            />
-          </div>
-          <div className="flex items-center justify-between">
-            <button
-              className="bg-slate-950 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
-              type="button"
-            >
-              Login
-            </button>
-          </div>
-        </form>
+    <div className="flex flex-col lg:flex-row justify-center items-center h-[90vh] relative">
+      <div className="lg:w-1/2 px-4 mb-8">
+        <BackButton link="/" />
+        <div className="bg-white shadow-md rounded px-8 pt-6 pb-8">
+          <h1 className="text-4xl font-bold mb-4 sm:mb-6 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text text-transparent">
+            Login
+          </h1>
+          <form>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="username"
+              >
+                Username
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="username"
+                type="text"
+                placeholder="Username"
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                type="password"
+                placeholder="Password"
+              />
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                className="bg-slate-950 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
+                type="button"
+              >
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      <div className="lg:w-1/2 px-4 hidden lg:block">
+        {/* Replace the src attribute with the URL of your desired image */}
+        <img
+          src="https://via.placeholder.com/400" // Replace this with the URL of your image
+          alt="Random Image"
+          className="rounded-lg"
+        />
       </div>
     </div>
   );

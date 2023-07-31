4a35044e4a79ec0c4e45a6b74c0926e1e2863cb4
Saurav Pant <pantsaurav005@gmail.com>
2023-07-31 22:23:14 +0530

    exhausted Splash Screen

--- src/app/Review/page.tsx
+++ src/app/Review/page.tsx
@@ -1,10 +1,11 @@
-"use client";
+"use client"
 import BackButton from "@/components/BackButton";
 import Link from "next/link";
 import React, { useState, useEffect } from "react";
 import { FaPlus } from "react-icons/fa";
 import axios from "axios";
-import { motion } from "framer-motion"; 
+import { motion } from "framer-motion";
+import SplashScreen from "@/components/SplashScreen";
 
 interface Review {
   id: number;
@@ -14,6 +15,7 @@ interface Review {
 
 const ReviewsPage: React.FC = () => {
   const [reviews, setReviews] = useState<Review[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [searchTerm, setSearchTerm] = useState<string>("");
 
   useEffect(() => {
@@ -21,9 +23,11 @@ const ReviewsPage: React.FC = () => {
       .get<{ reviews: Review[] }>("/api/Reviews")
       .then((response) => {
         setReviews(response.data.reviews);
+        setIsLoading(false);
       })
       .catch((error) => {
         console.error("Error fetching reviews:", error);
+        setIsLoading(false);
       });
   }, []);
 
@@ -33,39 +37,48 @@ const ReviewsPage: React.FC = () => {
 
   return (
     <div className="container mx-auto px-4 py-8 min-h-screen relative">
-      <BackButton link="/" />
-      <div className="flex justify-center mb-6">
-        <input
-          type="text"
-          placeholder="Search for a college..."
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          className="border border-gray-300 rounded-lg py-2 px-4"
-        />
-      </div>
-      <div className="grid gap-6 lg:grid-cols-3 md:grid-cols-2">
-        {filteredReviews.map((review) => (
-          <motion.div
-            key={review.id}
-            className="bg-white rounded-lg shadow-md p-6 h-60 flex flex-col justify-center hover:shadow-xl transition-shadow duration-300"
-            initial={{ opacity: 0, y: 50 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -50 }} 
-            transition={{ duration: 0.5 }} 
-          >
-            <h2 className="text-xl font-semibold mb-2">{review.collegeName}</h2>
-            <p className="text-gray-600">{review.review}</p>
-          </motion.div>
-        ))}
-      </div>
+      {/* Use the SplashScreen component as a wrapper */}
+      {isLoading ? (
+        <SplashScreen />
+      ) : (
+        <>
+          <BackButton link="/" />
+          <div className="flex justify-center mb-6">
+            <input
+              type="text"
+              placeholder="Search for a college..."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className="border border-gray-300 rounded-lg py-2 px-4"
+            />
+          </div>
+          <div className="grid gap-6 lg:grid-cols-3 md:grid-cols-2">
+            {filteredReviews.map((review) => (
+              <motion.div
+                key={review.id}
+                className="bg-white rounded-lg shadow-md p-6 h-60 flex flex-col justify-center hover:shadow-xl transition-shadow duration-300"
+                initial={{ opacity: 0, y: 50 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -50 }}
+                transition={{ duration: 0.5 }}
+              >
+                <h2 className="text-xl font-semibold mb-2">
+                  {review.collegeName}
+                </h2>
+                <p className="text-gray-600">{review.review}</p>
+              </motion.div>
+            ))}
+          </div>
 
-      <div className="fixed bottom-4 right-4">
-        <Link href="ReviewSubmission">
-          <button className="bg-gray-950 text-white rounded-full p-4 shadow-lg hover:bg-gray-700 transition-colors duration-300">
-            <FaPlus size={20} />
-          </button>
-        </Link>
-      </div>
+          <div className="fixed bottom-4 right-4">
+            <Link href="ReviewSubmission">
+              <button className="bg-gray-950 text-white rounded-full p-4 shadow-lg hover:bg-gray-700 transition-colors duration-300">
+                <FaPlus size={20} />
+              </button>
+            </Link>
+          </div>
+        </>
+      )}
     </div>
   );
 };

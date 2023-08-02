9f40a25d64349e7613a1295e98756b3a8022f75f
Saurav Pant <pantsaurav005@gmail.com>
2023-08-02 17:36:45 +0530

    middleware check

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,15 @@
-export default function middleware() {
-  console.log("middleware");
+import { NextRequest, NextResponse } from "next/server";
+
+export default function middleware(request: NextRequest) {
+  //   const path = request.nextUrl.pathname;
+
+  //   const token = request.cookies.get("token")?.value || "";
+  //   const { pathname } = request.nextUrl;
+  //   console.log(pathname);
+
+  //   if (!token) {
+
+  //   }
+
+  console.log("Saurav Pant");
 }

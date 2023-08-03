0e5a8c16ca636b573754195c898d10febe8e3edc
Saurav Pant <pantsaurav005@gmail.com>
2023-08-03 18:57:11 +0530

    Splash Screen added

--- src/app/Review/page.tsx
+++ src/app/Review/page.tsx
@@ -39,7 +39,6 @@ const ReviewsPage: React.FC = () => {
 
   return (
     <div className="container mx-auto px-4 py-8 min-h-screen relative">
-      {/* Use the SplashScreen component as a wrapper */}
       {isLoading ? (
         <SplashScreen />
       ) : (

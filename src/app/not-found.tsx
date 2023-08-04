ef651c3ce65116cdb4059101d8e52d1d27ab1101
Saurav Pant <pantsaurav005@gmail.com>
2023-08-04 16:06:06 +0530

    not found addition

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+type Props = {};
+
+const NotFound = (props: Props) => {
+  return (
+    //    Create a component for not found
+    <div></div>
+  );
+};
+
+export default NotFound;

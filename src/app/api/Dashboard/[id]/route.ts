e1bc52ef3f1679e96ad80c257f4d2df2735f1565
Saurav Pant <pantsaurav005@gmail.com>
2023-08-25 17:57:04 +0530

    PUT and delete methods

---
+++ src/app/api/Dashboard/[id]/route.ts
@@ -0,0 +1,9 @@
+export async function PUT(req: Request, res: Response) {
+  try {
+  } catch (err) {}
+}
+
+export async function DELETE(req: Request, res: Response) {
+  try {
+  } catch (err) {}
+}

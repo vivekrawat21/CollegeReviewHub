fda1c64749799b7f1b594dc7638bc83788a28023
Saurav Pant <pantsaurav005@gmail.com>
2023-08-26 20:10:12 +0530

    error handling

--- src/app/api/Dashboard/[id]/route.ts
+++ src/app/api/Dashboard/[id]/route.ts
@@ -1,9 +1,18 @@
+import { NextRequest, NextResponse } from "next/server";
 export async function PUT(req: Request, res: Response) {
   try {
-  } catch (err) {}
+  } catch (err) {
+    NextResponse.json({
+      message: "Not Updated",
+    });
+  }
 }
 
 export async function DELETE(req: Request, res: Response) {
   try {
-  } catch (err) {}
+  } catch (err) {
+    NextResponse.json({
+      message: "Not Deleted",
+    });
+  }
 }

40143878adcf5c214baa9f594a81eb015c12ebac
Saurav Pant <pantsaurav005@gmail.com>
2023-10-29 13:02:02 +0530

    signin with clerk

--- src/app/Signup/page.tsx
+++ src/app/Signup/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import { motion } from "framer-motion"; // Import Framer Motion
+import { motion } from "framer-motion"; 
 import Link from "next/link";
 import BackButton from "@/components/BackButton";
 import Images from "@/components/Images";

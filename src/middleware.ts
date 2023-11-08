adfbf14b351849cab25d1aacd137a46879140bdd
Saurav Pant <pantsaurav005@gmail.com>
2023-11-08 20:26:51 +0530

    middleware updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export default function middleware(request: NextRequest) {}
+export default function middleware(request: NextRequest) {
+  console.log("Saurav Pant");
+}
